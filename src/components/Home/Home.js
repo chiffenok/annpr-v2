7d87c8863c96a0916a48b1c9b5574adbcad2dc37
Anna Presnyakova <apresnyakova88@gmail.com>
2018-12-07 20:43:53 +0100

    fixing layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from './Home.module.scss';
-import woman from './../../common/img/woman.jpeg'
+import styles from "./Home.module.scss";
+import woman from "./../../common/img/woman.jpeg";
 
 const Home = () => {
     return (
@@ -8,26 +8,23 @@ const Home = () => {
             <aside className="photo">
                 <img src={woman} />
             </aside>
-            <article className="contentHello">
-                <hgroup>
-                    <h4 className={styles.greetingsTitle}>Hello,</h4>
-                    <h4 className={styles.greetingsTitle}>Let me tell about me</h4>
-                    <div className="circleMenu">
-                        <a href="#">My Resume</a>
-                        <a href="#">My work</a>
-                        <a href="#">My skils</a>
-                    </div>
-                    <p className="introParagraph">
-                        I'm a paragraph. Click here to add your own text and
-                        edit me. It’s easy. Just click “Edit Text” or double
-                        click me and you can start adding your own content and
-                        make changes to the font. Feel free to drag and drop me
-                        anywhere you like on your page. I’m a great place for
-                        you to write more. Tell a story and let your users get
-                        to know you.
-                    </p>
-                </hgroup>
-            </article>
+            <hgroup>
+                <h4 className={styles.greetingsTitle}>Hello,</h4>
+                <h4 className={styles.greetingsTitle}>a bit about me:</h4>
+            </hgroup>
+            <div className="circleMenu">
+                <a href="#">My Resume</a>
+                <a href="#">My work</a>
+                <a href="#">My skils</a>
+            </div>
+            <p className="introParagraph">
+                I'm a paragraph. Click here to add your own text and edit me.
+                It’s easy. Just click “Edit Text” or double click me and you can
+                start adding your own content and make changes to the font. Feel
+                free to drag and drop me anywhere you like on your page. I’m a
+                great place for you to write more. Tell a story and let your
+                users get to know you.
+            </p>
         </div>
     );
 };
