2398d6052a8aa8c5dcc0a22e0fc8f63182016c98
Anna Presnyakova <apresnyakova88@gmail.com>
2018-12-07 19:05:02 +0100

    styles for footer; -i font-awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Layout from './containers/Layout/Layout';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPhone, faEnvelope, faShareAlt, faCopyright} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPhone, faEnvelope, faShareAlt, faCopyright);
 
 class App extends Component {
     render() {
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Home from '../../components/Home/Home';
 import ss from '../../common/itcss.scss';
-import Header from "../../components/Header/Header";
+import Header from '../../components/Header/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Layout = () => {
     return (
@@ -14,18 +15,22 @@ const Layout = () => {
                 <div>
                     <ul>
                         <li>
+                            <span className="icon"><FontAwesomeIcon icon="phone" size="2x"/></span>
                             <h4>Call</h4>
                             <p>T: +49 177 18 99 365</p>
                         </li>
                         <li>
+                            <span className="icon"><FontAwesomeIcon icon="envelope" size="2x"/></span>
                             <h4>Contact</h4>
                             <p><a href="mailto:apresnyakova88@gmail.com">apresnyakova88@gmail.com</a></p>
                         </li>
                         <li>
+                            <span className="icon"><FontAwesomeIcon icon="share-alt" size="2x"/></span>
                             <h4>Follow Me</h4>
                             <p></p>
                         </li>
                         <li>
+                            <span className="icon"><FontAwesomeIcon icon="copyright" size="2x"/></span>
                             <p>2019 Anna Presnyakova</p>
                         </li>
                     </ul>
