23e77f7f8d9c913430c5096185b142b402e5a0ff
Anna Presnyakova <apresnyakova88@gmail.com>
2018-12-17 16:14:39 +0100

    Updates

---
+++ precache-manifest.4ebe8f7a028c94e32f505724266e446b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee326adb8cc81f68b20a",
+    "url": "/static/css/main.668bdaa3.chunk.css"
+  },
+  {
+    "revision": "ee326adb8cc81f68b20a",
+    "url": "/static/js/main.ee326adb.chunk.js"
+  },
+  {
+    "revision": "7bc9ab364d770d0bbb67",
+    "url": "/static/css/1.a8d358ef.chunk.css"
+  },
+  {
+    "revision": "7bc9ab364d770d0bbb67",
+    "url": "/static/js/1.7bc9ab36.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b39cdab342430f10d6f237d822cc1838",
+    "url": "/static/media/woman.b39cdab3.jpeg"
+  },
+  {
+    "revision": "061cd350e91a27df32b95c8457bfc532",
+    "url": "/index.html"
+  }
+];
